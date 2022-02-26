287d35335ba33fe89b78f901fe5e4d430ba5995f
Yama <92165034+yanmada@users.noreply.github.com>
2022-02-25 21:54:52 -0300

    atualizando novo exercício

---
+++ exercicios/ex014.py
@@ -0,0 +1,11 @@
+# Minha solução
+c = float(input('Informe a temperatura em ºC: '))
+f = (c * 1.8) + 32
+print('A temperatura de {}ºC corresponde a {}ºF!'.format(c, f))
+
+# Solução Gustavo Guanabara (CursoemVideo)
+# c = float(input('Informe a temperatura em ºC: '))
+# f = 9 * c / 5 + 32
+# OU
+# f = ((9 * c) / 5) + 32
+# print('A temperatura de {}ºC corresponde a {}ºF'.format(c, f))
---
+++ exercicios/ex015.py
@@ -0,0 +1,11 @@
+# Minha solução
+dias = int(input('Quantos dias alugados? '))
+km = float(input('Quantos Km rodados? '))
+tot = (dias * 60) + (km * 0.15)
+print('O total a pagar é de R${:.2f}'.format(tot))
+
+# Solução Gustavo Guanabara (CursoemVideo)
+# dias = int(input('Quantos dias alugados? '))
+# km = float(input('Quantos Km rodados? '))
+# pago = (dias * 60) + (km * 0.15)
+# print('O total a pagar é de R${:.2f}'.format(pago))
