e509b700b7070cce9745ecc00f37a97f130e9f4c
Yama <92165034+yanmada@users.noreply.github.com>
2022-03-03 19:27:51 -0300

    atualizando exercício 023

--- exercicios/ex023.py
+++ exercicios/ex023.py
@@ -1,7 +1,19 @@
-# Ainda Fazendo
-num = input('Informe um número: ')
-print('Analisando o número {}'.format(num))
-print('Unidade: {}')
-print('Dezena: {}')
-print('Centena: {}')
-print('Milhar: {}')
+# Minha solução (Jeito errado pois só reconhece 4 algarismos!)
+'''
+num = str(input('Informe um número: '))
+print('Unidade: {}'.format(num[3]))
+print('Dezena: {}'.format(num[2]))
+print('Centena: {}'.format(num[1]))
+print('Milhar: {}'.format(num[0]))
+'''
+
+# Solução Gustavo Guanabara
+num = int(input('Informe um número: '))
+u = num // 1 % 10
+d = num // 10 % 10
+c = num // 100 % 10
+m = num // 1000 % 10
+print('Unidade: {}'.format(u))
+print('Dezena: {}'.format(d))
+print('Centena: {}'.format(c))
+print('Milhar: {}'.format(m))
