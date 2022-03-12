c6db5909cda02bbbef3c6028a36f78d4164340e0
Yama <92165034+yanmada@users.noreply.github.com>
2022-03-11 21:45:23 -0300

    terminando exercício

--- exercicios/ex036.py
+++ exercicios/ex036.py
@@ -1,6 +1,18 @@
 # Ainda fazendo
-casa = float(input('Valor da casa: R$'))
+valorcasa = float(input('Valor da casa: R$'))
 sal = float(input('Salário do comprador: R$'))
 ano = int(input('Quantos anos de financiamento? '))
+print(f'Para pagar uma casa de R${valorcasa:.2f} em {ano} anos a prestação será de R${sal:.2f}')
+
+# Escreva um programa para aprovar o empréstimo bancário para a compra de uma casa.
+# Pergunte o valor da casa, o salário do comprador e em quantos anos ele vai pagar.
+# A prestação mensal não pode exceder 30% do salário ou então o empréstimo será negado.
 
 
+
+'''
+if :
+    print('Empréstimo pode ser CONCEDIDO!')
+else:
+    print('Empréstimo NEGADO!')
+'''
--- exercicios/ex037.py
+++ exercicios/ex037.py
@@ -1,13 +1,28 @@
-# Ainda fazendo
+# Minha solução
+
+cores = {
+    'limpar': '\033[m',
+    'verde': '\033[0;32m',
+    'amarelo': '\033[0;33m',
+    'roxo': '\033[0;35m'
+}
+
+l = cores['limpar']
+vd = cores['verde']
+am = cores['amarelo']
+rx = cores['roxo']
+
 num = int(input('Digite um número inteiro: '))
 print('Escolha uma das bases para conversão:')
-print('[ 1 ] Converter para BINÁRIO')
-print('[ 2 ] Converter para OCTAL')
-print('[ 3 ] Converter para HEXADECIMAL')
-opcao = int(input('Sua opção: '))
-if opcao == 1:
-    print('{} convertido para BINÁRIO é igual a {}'.format(num, bin(num)))
-elif opcao == 2:
-    print('{} convertido para OCTAL é igual a {}'.format(num, oct(num)))
+print(f'[ {rx}1{l} ] Converter para BINÁRIO')
+print(f'[ {rx}2{l} ] Converter para OCTAL')
+print(f'[ {rx}3{l} ] Converter para HEXADECIMAL')
+base = int(input('Sua opção: '))
+if base >= 4:
+    print('[ERRO] Conversão inválida!')
+elif base == 1:
+    print(f'{num} convertido para {am}BINÁRIO{l} é igual a {vd}{bin(num)[2:]}{l}')
+elif base == 2:
+    print(f'{num} convertido para {am}OCTAL{l} é igual a {vd}{oct(num)[2:]}{l}')
 else:
-    print('{} convertido para HEXADECIMAL é igual a {}'.format(num, hex(num)))
+    print(f'{num} convertido para {am}HEXADECIMAL{l} é igual a {vd}{hex(num)[2:]}{l}')
