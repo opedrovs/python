f72448cbaffc4cc254af2fa746d5faaa5c629cbb
Yama <92165034+yanmada@users.noreply.github.com>
2022-04-25 20:41:53 -0400

    atualizando exercício de dicionários

--- exercicios/ex094.py
+++ exercicios/ex094.py
@@ -1,3 +1,4 @@
+# Minha solução
 cadastro = {}
 lista = []
 soma = media = 0
@@ -10,7 +11,6 @@ while True:
     cadastro['idade'] = int(input('Idade: '))
     soma += cadastro['idade']
     lista.append(cadastro.copy())
-    media += soma / len(lista)
     resp = str(input('Quer continuar? [S/N] ')).strip().upper()
     while resp != 'S' and resp != 'N':
         print('ERRO! Responda apenas S ou N.')
@@ -18,9 +18,18 @@ while True:
     if resp == 'N':
         break
 print('-=' * 30)
-print(lista)
 print(f'A) Ao todo temos {len(lista)} pessoas cadastradas.')
+media = soma / len(lista)
 print(f'B) A média de idade é de {media:.2f} anos.')
 print(f'C) As mulheres cadastradas foram ', end='')
-for pessoa in lista:
-    print(pessoa)
+for pessoas in lista:
+    if pessoas['sexo'] == 'F':
+        print(f'{pessoas["nome"]} ', end='')
+print()
+print(f'D) Lista das pessoas que estão acima da média:')
+for pessoas in lista:
+    if pessoas['idade'] > media:
+        for chave, pessoa in pessoas.items():
+            print(f'{chave} = {pessoa}; ', end='')
+        print()
+print('<< ENCERRADO >>')
